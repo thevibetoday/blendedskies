e0b7907e84d1673669179f91108fd43938d325c0
thevibetoday <info@thevibetoday.com>
2025-04-04 18:34:28 -0400

    Create questionnaire.js

---
+++ questionnaire.js
@@ -0,0 +1,19 @@
+// Simple option selection script
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all option list items
+    const optionItems = document.querySelectorAll('.options li');
+    
+    // Add click event listener to each option
+    optionItems.forEach(item => {
+        item.addEventListener('click', function() {
+            // Remove 'selected' class from all siblings
+            const siblings = Array.from(this.parentNode.children);
+            siblings.forEach(sibling => {
+                sibling.classList.remove('selected');
+            });
+            
+            // Add 'selected' class to clicked item
+            this.classList.add('selected');
+        });
+    });
+});
